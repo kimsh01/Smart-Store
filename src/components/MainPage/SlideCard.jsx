1cdac76d3821110dac4418ca75e56dee174a427d
kimsh01 <rlatjsgur01@gmail.com>
2023-01-29 03:04:01 +0900

    <Slider> 자꾸 오류남 이유를 1도 모르겠음

--- src/components/MainPage/SlideCard.jsx
+++ src/components/MainPage/SlideCard.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 import Sdata from './Sdata';
 
 const SlideCard = () => {
+     const settings = {
+             dots: true,
+             infinite: true,
+             slidesToShow: 1,
+             slidesToScroll: 1,
+             autoplay: true,
+              appendDots: (dots) => {
+              return <ul style={{ margin: "0px" }}>{dots}</ul>
+          },
+     }  
     return (
         <>
-            {Sdata.map((value, index) =>{
-                return(
-                <div className="box d_flex top" key={index}>
-                    <div className="left">
-                        <h1>{value.title}</h1>
-                        <p>{value.desc}</p>
-                        <button className='btn-primary'>Visit Collections</button>
+             <Slider {...settings}>
+                {Sdata.map((value, index) =>{
+                    return(
+                    <div className="box d_flex top" key={index}>
+                        <div className="left">
+                            <h1>{value.title}</h1>
+                            <p>{value.desc}</p>
+                            <button className='btn-primary'>Visit Collections</button>
+                        </div>
+                        <div className="right">
+                            <img className='box_img' src={value.cover} alt='' />
+                        </div>
                     </div>
-                    <div className="right">
-                        <img src={value.cover} alt='' />
-                    </div>
-                </div>
-            )
-        })}
+                )
+            })}
+             </Slider>  
         </>
     );
 };
